fb412655e9519ef38059865536c7f4f9b741b61d
Saksham Bangale <sakshambangale779@gmail.com>
2025-05-10 23:41:39 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/saksham_app/MainActivity.java
+++ app/src/main/java/com/example/saksham_app/MainActivity.java
@@ -10,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
